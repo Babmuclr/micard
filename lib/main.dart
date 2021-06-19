8fa53d6a7930481e42a380bcfaf5023ec3b28b12
Babmuclr <it.onepiece.balsreal@gmail.com>
2021-06-19 20:47:59 +0900

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,86 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/mypic.jpg'),
+              ),
+              Text(
+                'Takumi Inui',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'KYOTO UNIVERSITY STUDENT',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Source Sans Pro',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 300.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+81 090-1234-5678',
+                    style: TextStyle(
+                      fontFamily: 'Sans Source Pro',
+                      color: Colors.teal,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'takumi.inui@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Sans Source Pro',
+                      color: Colors.teal,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
